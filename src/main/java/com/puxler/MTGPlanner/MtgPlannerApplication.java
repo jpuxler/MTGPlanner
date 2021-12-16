a90d56eca0141cecea405eb4056b25aa6e2c2471
JonathanPuxler <software@puxler.de>
2021-12-16 19:03:45 +0100

    initial commit

---
+++ src/main/java/com/puxler/MTGPlanner/MtgPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.puxler.MTGPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/puxler/MTGPlanner/MtgPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puxler.MTGPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
